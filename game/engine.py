66bc831fa5920cea039102cef2952bd5a9518875
Simonas Grubinskas <simonas.grubinskas@gmail.com>
2020-01-20 00:22:52 +0200

    Add Game class. Add Players and Deck to the Game

--- game/engine.py
+++ game/engine.py
@@ -2,6 +2,8 @@ import random
 from collections import namedtuple
 
 Card = namedtuple('Card', ['rank', 'suit'])
+Player = namedtuple('Player', ['hole_cards', 'stack'])
+
 RANKS = [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
 SUITS = ['h', 'd', 'c', 's']
 
@@ -14,3 +16,7 @@ class Deck():
         card = random.choice(self.cards)
         return card
 
+class Game():
+    def __init__(self, players):
+        self.players = tuple(Player([], 100) for index in range(players))
+        self.deck = Deck()
--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,6 +1,6 @@
 import pytest
 
-from game.engine import Deck
+from game.engine import Deck, Game
 
 def test_deck():
     deck = Deck()
@@ -10,3 +10,17 @@ def test_deck_draw():
     deck = Deck()
     assert deck.draw().rank in list(range(2, 15))
     assert deck.draw().suit in ['h', 'd', 'c', 's']
+
+def test_game():
+    game = Game(players=2)
+    assert len(game.players)
+
+def test_player():
+    game = Game(players=2)
+    second_player = game.players[1]
+    assert second_player.stack == 100
+
+def test_game_has_deck():
+    game = Game(players=2)
+    deck = game.deck
+    assert len(deck.cards)
