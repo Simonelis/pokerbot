08011eae80961339242e8bb019a92569b054d53f
Simonas Grubinskas <simonas.grubinskas@gmail.com>
2020-01-20 00:12:08 +0200

    Add drawing capability to a Deck class

--- game/engine.py
+++ game/engine.py
@@ -1,4 +1,16 @@
+import random
+from collections import namedtuple
+
+Card = namedtuple('Card', ['rank', 'suit'])
+RANKS = [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+SUITS = ['h', 'd', 'c', 's']
+
 class Deck():
 
     def __init__(self):
-        self.cards = ['card' for card in range(52)]
+        self.cards = [Card(rank, suit) for rank in RANKS for suit in SUITS]
+
+    def draw(self):
+        card = random.choice(self.cards)
+        return card
+
--- tests/test_engine.py
+++ tests/test_engine.py
@@ -6,6 +6,7 @@ def test_deck():
     deck = Deck()
     assert len(deck.cards) == 52
 
-# def test_deck_draw():
-#     deck = Deck()
-#     assert deck.draw().rank is in list(range(13))
+def test_deck_draw():
+    deck = Deck()
+    assert deck.draw().rank in list(range(2, 15))
+    assert deck.draw().suit in ['h', 'd', 'c', 's']
